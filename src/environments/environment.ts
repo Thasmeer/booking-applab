51f942a37a2f19ab08a752f5b702aa2b02ae69d7
Thasmeer P <thasmeerp@gmail.com>
2021-07-29 14:48:37 +0530

    chor: env changes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: true,
-  API_URL: 'https://booking-applab-api.herokuapp.com/'
+  production: false,
+  API_URL: 'https://booking-applab-api.herokuapp.com'
 };
 
 /*
